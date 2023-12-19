6777c11edbab538d700a8f37d8335e7fa497212b
HOSAIN MAHMUD <hosainmahmud191@gmail.com>
2023-12-20 05:50:20 +0600

    playlist song remove option created

--- search.php
+++ search.php
@@ -67,9 +67,10 @@ if (isset($_GET['term'])) {
                 <span class='artistName'>" . $albumArtist->getName() . "</span>
             </div>
 
-            <div class= 'trackOption'>
-                <img class='optionButton' src ='assets/images/icons/more.png'>
-            </div>
+            <div class='trackOptions'>
+						<input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+						<img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
+					</div>
 
             <div class='trackDuration'>
                 <span class='duration'>" . $albumSong->getDuration() . "</span>
@@ -157,3 +158,8 @@ if (isset($_GET['term'])) {
     ?>
 
 </div>
+
+<nav class="optionsMenu">
+	<input type="hidden" class="songId">
+	<?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+</nav>
